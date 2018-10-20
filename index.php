6bade95079cfbed5f8e3f8f9b40fefc03be89abb
Lacey Sikes <lsikes@csumb.edu>
2018-10-20 23:17:44 +0000

    Initial chicken of lab 1

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
-<html><body>
+<html>
+<head>
+    Home
+    About
+    Contact
+</head>    
+<body>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo "CST336 Home Page for Lacey Sikes";
+echo "Hello! Thank you for visiting my page!";
+echo "I am a Senior Software Engineer Student at CSUMB.";
+echo "I enjoy working with Linux and Python";
+echo "\nFeel free to contact me!";
+echo "\nThere are 10 ways there are 10 types of people in this world,\n";
+echo "Those who know binary and those who don't";
+echo "\n-Unknown";
 
 ?>
 </body>
