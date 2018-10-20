268e5eb416f64f6bea3a2e7d8ca2235f811a6d9c
Lacey Sikes <lsikes@csumb.edu>
2018-10-20 21:52:15 +0000

    Initial chicken of the index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html><body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+echo "CST336 Home Page for Lacey Sikes";
+
+?>
+</body>
+</html>
