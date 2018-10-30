3a41cd15956022ee4dc288b5a99915c34932c918
Lacey Sikes <lsikes@csumb.edu>
2018-10-30 19:07:08 +0000

    chicken of 777 into Lab2 directory

---
+++ Labs/Lab2/777/index.php
@@ -0,0 +1,26 @@
+<?php
+        
+include 'inc/functions.php';
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
+    </head>
+    <body>
+        <div id="main"></div>
+            <?php
+                play();
+            ?>
+        
+            <form>
+                <input type="submit" value="Spin!"/>
+            </form>
+        </div>
+            
+    </body>
+</html>
