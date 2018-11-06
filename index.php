e009911768bccc947967e7da724334c3c5bbeb88
Lacey Sikes <lsikes@csumb.edu>
2018-11-06 18:30:11 +0000

    chicken of initial page

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
 <html>
-<head>
-    Home
-    About
-    Contact
-</head>    
 <body>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo "Hello! Thank you for visiting my page!";
+echo "Hello! Thank you for visiting my page!\n\n";
 echo "I am a Senior Software Engineer Student at CSUMB.";
 echo "I enjoy working with Linux and Python";
 echo "\nFeel free to contact me!";
-echo "\nThere are 10 ways there are 10 types of people in this world,\n";
+echo "\n\n\nThere are 10 ways there are 10 types of people in this world,\n";
 echo "Those who know binary and those who don't";
 echo "\n-Unknown";
 
