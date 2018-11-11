69bb7c0d8ff3274980763d813831e920de8ec576
Lacey Sikes <lsikes@csumb.edu>
2018-11-11 05:13:31 +0000

    chicken of form order

--- HW/HW3/setSession.php
+++ HW/HW3/setSession.php
@@ -2,20 +2,19 @@
 //Check whether the form has been submitted
 if (array_key_exists('check_submit', $_POST)) {
    //Converts the new line characters (\n) in the text area into HTML line breaks (the <br /> tag)
-   $_POST['Comments'] = nl2br($_POST['Comments']); 
+   $_POST['comments'] = nl2br($_POST['comments']); 
    //Check whether a $_GET['Languages'] is set
-   if ( isset($_POST['Colors']) ) { 
-     $_POST['Colors'] = implode(', ', $_POST['Colors']); //Converts an array into a single string
+   if ( isset($_POST['likes']) ) { 
+     $_POST['likes'] = implode(', ', $_POST['likes']); //Converts an array into a single string
    }
 
    //Let's now print out the received values in the browser
-   echo "Your name: {$_POST['Name']}<br />";
-   echo "Your password: {$_POST['Password']}<br />";
-   echo "Your favourite season: {$_POST['Seasons']}<br /><br />";
-   echo "Your comments:<br />{$_POST['Comments']}<br /><br />";
-   echo "You are from: {$_POST['Country']}<br />";
-   echo "Colors you chose: {$_POST['Colors']}<br />";
+   echo "Your name: {$_POST['name']}<br />";
+   echo "Your business: {$_POST['state']}<br />";
+   echo "Your maturity: {$_POST['maturity']}<br /><br />";
+   echo "You like:<br />{$_POST['likes']}<br /><br />";
+   echo "You enjoy:<br />{$_POST['comments']},br /><br />";
 } else {
-    echo "You can't see this page without submitting the form.";
+    echo "You need to fill out the form to see a change.";
 }
 ?>
