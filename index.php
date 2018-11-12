cfe5b4809e49404522f21e9744e7a18c43067515
Lacey Sikes <lsikes@csumb.edu>
2018-11-12 22:27:19 +0000

    chicken of cst336 home page modifications

--- index.php
+++ index.php
@@ -1,18 +1,70 @@
+
 <html>
-<body>
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
+    <head>
+        
+        <style type="text/css">
+            header {
+                text-align: center;
+            }
+            body {
+                background-color: #000000;
+                color: #f40480;
+            }
+            
+            /*a {
+                color: #ffffff;
+            }*/
+        
+            #labs, #homework {
+                text-align: center;
+                /*float: none;
+                padding-left: 350px;*/
+            }
+            
+            #footer {
+                text-align: center;
+            }
+        </style>
+        
+    </head>    
 
-echo "Hello! Thank you for visiting my page!\n\n";
-echo "I am a Senior Software Engineer Student at CSUMB.";
-echo "I enjoy working with Linux and Python";
-echo "\nFeel free to contact me!";
-echo "\n\n\nThere are 10 ways there are 10 types of people in this world,\n";
-echo "Those who know binary and those who don't";
-echo "\n-Unknown";
 
-?>
-</body>
+    <body>
+        <header>
+             <h1><strong>Lacey Sikes's</strong> CST-336 Home Page</h1>   
+        </header>
+
+    
+        <div id="content">
+            <div id="labs">
+                <h2>Lab Assignments</h2>
+                <a href="Labs/Lab1/index.html">Lab 1 </a><br>
+                <a href="Labs/Lab2/777/index.php"> Lab 2</a><br>
+                <a href="Labs/Lab3/index.php">Lab 3</a><br>
+                <a href="Labs/Lab4/index.php">Lab 4</a><br>
+                
+            </div>
+        
+            <div id="homework">
+                <h2>Homework Assignments</h2>
+                <a href="HW/HW1/index.html">Homework 1</a><br>
+                <a href="HW/HW2/index.php">Homework 2</a><br>
+                <a href="HW/HW3/index.php">Homework 3</a><br>
+            </div>
+        </div>
+
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer id="footer">
+            <hr>
+            Internet Programming. 2018&copy; Sikes <br />
+            <strong>Disclaimer:</strong> The information in this webpage is fictitous. <br />
+            It is used for academic purposes only.
+            <figure id="csumbLogo">
+                <img src="img/csumbLogo.jpg" alt="Logo for CSUMB" width="50"/>
+            </figure>
+        </footer>
+        <!-- closing footer -->
+    
+    </body>
 </html>
