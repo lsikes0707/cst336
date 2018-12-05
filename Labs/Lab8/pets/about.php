eeea978a1a0f93d6193d264ac2dc4f51e1d87465
Lacey Sikes <lsikes@csumb.edu>
2018-12-05 06:47:52 +0000

    chickenen an about section

---
+++ Labs/Lab8/pets/about.php
@@ -0,0 +1,38 @@
+<?php
+  include 'inc/header.php';
+?>
+    
+    <br /><br />
+    <hr>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> CSUMB: Pet Shelter </title>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+        <h2> Or rather, "unofficial", Pet Shelter</h2>
+        <h3>
+            This is a fictious Pet Shelter for academic purposes only.
+        </h3>
+        
+        <style>
+            body {
+                text-align: center;
+            }
+        </style>
+   
+    </head>
+    <body>
+
+    </body>
+    <footer>
+      Disclaimer: The information in this site is not real.
+    </footer>
+</html>
+
